d00bd2a82a311af1794b7c934eab4f21efd76024
James Butler <james.butler@sandfox.co.uk>
2016-01-29 13:57:04 +0000

    bump deps add auto-task version magic for slack messages some linting fixes

--- test/utils/taskFactory.js
+++ test/utils/taskFactory.js
@@ -9,8 +9,8 @@ module.exports = function create (opts) {
   task.command = opts.command || ['echo', ': task start', '&&', 'echo', ':PID $$', '&&', 'sleep', '60', '&&', 'echo', ': task ended']
   task.tarball = opts.tarball || 'http://127.0.0.1:12345/app.tar.gz?somestuff=yeeaah'
   task.enviroment = opts.enviroment || {FOO: 'BAR'}
-    task.meta = opts.meta || {
-      version: 'this-is-a-test-task-version'
-    }
+  task.meta = opts.meta || {
+    version: 'this-is-a-test-task-version'
+  }
   return task
 }
